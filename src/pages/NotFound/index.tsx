b465fae15ad9918be81f13624155ce0e388c9d7b
Leonardo Felipe Sarmento dos Reis <leonardo.felipesr@gmail.com>
2024-10-28 14:14:42 -0300

    feat not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
+import styles from './NotFound.module.css';
+import { LanguageContext } from '../../contexts/LanguageContext';
+import { notFoundTranslate } from '../../utils/LanguagesDictionary/NotFoundTranslation';
+import error from '../../assets/error.png';
+
 export const NotFound = () => {
+  const { language } = useContext(LanguageContext);
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for could not be found.</p>
+    <div className={styles.notFound}>
+      <div>
+        <img src={error} alt="page not found" />
+      </div>
+      <p>{notFoundTranslate(language)}</p>
     </div>
   );
 };
